70084d0c38bcd28eced6853150dcb13751320a58
shoeb5401 <shoeb.qureshi.5401@gmail.com>
2025-12-17 18:42:09 +0530

    Fix the file structure

---
+++ Beginner/directory_op.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from datetime import date
+
+dir_path = input("Enter the Path of the Directory: ")
+p = Path(dir_path)
+
+if p.exists() and p.is_dir():
+    print("\nPrinting all files and folders in the directory:")
+    for file in p.iterdir():
+        print(file)
+
+    print("\nPrinting files older than 7 days:")
+    for file in p.iterdir():
+        file_date = date.fromtimestamp(file.stat().st_mtime)
+        if (date.today() - file_date).days > 7:
+            print(file)
+            
+
+else:
+    print("Enter a valid directory path.")
--- Beginner/file_check.py
+++ Beginner/file_check.py
@@ -1,13 +1,14 @@
-import os 
+import os , pathlib
 
 
 file_path = str(input("Input a file path, You want to Check:"))
-exist = os.path.exists(file_path)
+# exist = os.path.exists(file_path)
+exist = pathlib.Path.is_file(file_path)
 print(f"Your file path is: {file_path}\n")
 if exist :
     print("Your file path exist")
 elif file_path.startswith(('/','.','..')):
-    print("Your Path doesnot exist\n")
+    print("Your file Path doesnot exist\n")
     
     ans = str(input("Do you want to create a file in this path(Y or N):\n"))
     if ans == 'Y' or 'y':
