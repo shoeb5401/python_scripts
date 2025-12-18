91580d1d19dfab0433cd49d83fd56fcb220be54f
shoeb5401 <shoeb.qureshi.5401@gmail.com>
2025-12-18 07:28:57 +0530

    Modified env Script

--- Beginner/env_reader.py
+++ Beginner/env_reader.py
@@ -1,10 +1,13 @@
 import os 
 
-custom_env = str(input("Input the env variable you want to search: "))
+custom_env = str(input("Input the env variable you want to search: ")).upper()
 
-envs = os.environ
 
-for (env, val) in envs.items():
-    if custom_env.lower() in env.lower():
-        print(f"{env}: {val}")
-    # print(f"{env}: {val}")
+if os.getenv(custom_env) == None:
+    print("Enter a Valid ENV variable")
+else:
+    print(f"{custom_env}: {os.getenv(custom_env)}")
+# for (env, val) in envs.items():
+#     if custom_env.lower() in env.lower():
+#         print(f"{env}: {val}")
+#     # print(f"{env}: {val}")
